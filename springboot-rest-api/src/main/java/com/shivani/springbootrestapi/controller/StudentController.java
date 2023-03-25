4b5f0017d6c483d49fa264f0e982a02d4d16070c
shivanip1512 <shivanipacharne10@gmail.com>
2023-03-26 03:12:57 +0530

    REST API : returns List, PathVariable

--- springboot-rest-api/src/main/java/com/shivani/springbootrestapi/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/shivani/springbootrestapi/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.shivani.springbootrestapi.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +18,14 @@ public class StudentController {
 		return new Student(1, "ABC", "XYZ");
 	}
 
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		return List.of(new Student(1, "A", "B"), new Student(2, "C", "D"), new Student(3, "E", "F"));
+	}
+	
+	//spring-boot rest endpoint with path-variable
+	@GetMapping("/{id}")
+	public Student getStudent(@PathVariable int id) {
+		return new Student(id, "Joe", "Smith");
+	}
 }
