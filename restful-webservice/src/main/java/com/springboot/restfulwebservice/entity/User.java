860c95c82fa340e726e014ef3d4c38076e350f03
shivanip1512 <shivanipacharne10@gmail.com>
2023-03-28 00:28:38 +0530

    Microservice : Usage of JPA

---
+++ restful-webservice/src/main/java/com/springboot/restfulwebservice/entity/User.java
@@ -0,0 +1,30 @@
+package com.springboot.restfulwebservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+}
