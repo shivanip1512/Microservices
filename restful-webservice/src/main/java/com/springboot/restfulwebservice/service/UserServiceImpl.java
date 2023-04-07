15327aed82ba010e414ad58add5d43fe9fa27e0f
shivanip1512 <shivanipacharne10@gmail.com>
2023-04-08 01:14:30 +0530

    Using ModelMapper Library

--- restful-webservice/src/main/java/com/springboot/restfulwebservice/service/UserServiceImpl.java
+++ restful-webservice/src/main/java/com/springboot/restfulwebservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.springboot.restfulwebservice.dto.UserDTO;
@@ -16,26 +17,36 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-	
+
 	private UserRepository userRepository;
+	private ModelMapper modelMapper;
 
 	@Override
 	public UserDTO createUser(UserDTO userDto) {
-		User user = UserMapper.mapToUser(userDto);
+		// User user = UserMapper.mapToUser(userDto);
+		User user = modelMapper.map(userDto, User.class);
+
 		User savedUser = userRepository.save(user);
-		return UserMapper.mapToUserDto(savedUser);
+
+		// return UserMapper.mapToUserDto(savedUser);
+		return modelMapper.map(savedUser, UserDTO.class);
 	}
 
 	@Override
 	public UserDTO getUser(Integer userId) {
 		Optional<User> optionalUser = userRepository.findById(userId);
-		return optionalUser.isPresent() ? UserMapper.mapToUserDto(optionalUser.get()) : null;
+		// return optionalUser.isPresent() ? UserMapper.mapToUserDto(optionalUser.get())
+		// : null;
+		return optionalUser.isPresent() ? modelMapper.map(optionalUser.get(), UserDTO.class) : null;
 	}
 
 	@Override
 	public List<UserDTO> getAll() {
 		List<User> users = userRepository.findAll();
-		return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+		// return
+		// users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+		return users.stream().map(user -> modelMapper.map(user, UserDTO.class)).collect(Collectors.toList());
+
 	}
 
 	@Override
@@ -44,7 +55,8 @@ public class UserServiceImpl implements UserService {
 		if (existingUserDto != null) {
 			existingUserDto.setName(user.getName());
 			existingUserDto.setEmail(user.getEmail());
-			userRepository.save(UserMapper.mapToUser(existingUserDto));
+			// userRepository.save(UserMapper.mapToUser(existingUserDto));
+			userRepository.save(modelMapper.map(existingUserDto, User.class));
 		}
 		return existingUserDto;
 	}
