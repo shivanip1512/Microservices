ded67fb679da9097f671c6e755a8f6466acac8b9
shivanip1512 <shivanipacharne10@gmail.com>
2023-04-21 04:39:43 +0530

    Department - Added dao

---
+++ springcloud-microservices/department-service/src/main/java/com/java/microservice/departmentservice/entity/Department.java
@@ -0,0 +1,19 @@
+package com.java.microservice.departmentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+}
---
+++ springcloud-microservices/department-service/src/main/java/com/java/microservice/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.java.microservice.departmentservice.repository;
+
+import com.java.microservice.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
