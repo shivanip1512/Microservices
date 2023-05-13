8d43e11b2d3483b60107dcce821752cf5281d281
shivanip1512 <shivanipacharne10@gmail.com>
2023-05-13 20:09:24 +0530

    Service Registry - Spring Cloud Netflix Eureka and Spring Loadbalancer

--- springcloud-microservices/employee-service/src/main/java/com/java/microservice/employeeservice/service/APIClient.java
+++ springcloud-microservices/employee-service/src/main/java/com/java/microservice/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", name = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{dept-code}")
     DepartmentDTO getDepartmentByDepartmentCode(@PathVariable("dept-code") String departmentCode);
